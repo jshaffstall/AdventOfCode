705041cd0f1b9f0338fe5ebdff6991ac4426839b
Jay Shaffstall <jshaffstall@gmail.com>
2021-12-01 14:51:15 -0500

    Create Day04A.py

---
+++ 2015/Day04A.py
@@ -0,0 +1,15 @@
+import sys
+import hashlib
+
+line = input().strip()
+current = 1
+
+while True:
+    hash = hashlib.md5((line+str(current)).encode('utf-8')).hexdigest()
+    
+    if hash.startswith('00000'):
+        print(current)
+        sys.exit()
+        
+    current += 1
+        
