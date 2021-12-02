8e16f99cdc7aafbe35d7a6e50adb40b6971891c1
Jay Shaffstall <jshaffstall@gmail.com>
2021-12-02 17:52:38 -0500

    Create Day08A.py

---
+++ 2015/Day08A.py
@@ -0,0 +1,44 @@
+# Not there yet, still needs work
+
+import sys
+code = 0
+characters = 0
+
+for line in sys.stdin:
+    line = line.strip()
+    
+    if line == '-1':
+        break
+    
+    code += len(line)
+    
+    while True:
+        index = line.find('\\\\')
+        
+        if index == -1:
+            break
+        
+        characters += 1
+        line = line[:index]+line[index+2:]
+
+    while True:
+        index = line.find('\\"')
+        
+        if index == -1:
+            break
+        
+        characters += 1
+        line = line[:index]+line[index+2:]
+
+    while True:
+        index = line.find('\\x')
+        
+        if index == -1:
+            break
+        
+        characters += 1
+        line = line[:index]+line[index+4:]
+
+    characters += len(line)-2
+
+print(code-characters)
