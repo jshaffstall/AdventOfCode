477c3d6441143f28672e139ac7d3d6667fa3ec72
Jay Shaffstall <jshaffstall@gmail.com>
2021-12-12 12:00:44 -0500

    Create Day03B.py

---
+++ 2017/Day03B.py
@@ -0,0 +1,3 @@
+# Nothing against mathmatically based problems, but ones that 
+# require no programming are not really my thing.  This one's 
+# a known number sequence, with answers at https://oeis.org/A141481/b141481.txt
